55c3801948452ddfbe5e6f10ea3577e58bf119a7
bcasale <benjamin.casale@proton.me>
2025-08-19 23:40:00 +0200

    chore: add build.rs

---
+++ build.rs
@@ -0,0 +1,10 @@
+fn main() {
+    if std::env::var("PROFILE").unwrap() == "release" {
+        println!("cargo:rustc-env=MYAPP_DB_PATH=/usr/share/myapp/units.db");
+    } else {
+        println!(
+            "cargo:rustc-env=DB_PATH={}/data/sqlite.db",
+            env!("CARGO_MANIFEST_DIR")
+        );
+    }
+}
